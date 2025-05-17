88a01a3839fc67c5ef0bcc0e28e066f6f277b56f
Vanshahuja1 <139003501+Vanshahuja1@users.noreply.github.com>
2025-05-17 15:33:30 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json())
 app.use(cookieparser())
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:5173'  
+    origin: 'https://authentication-plugin-4.onrender.com'  
 }));
 
 app.use('/api/auth',AuthRoutes)
